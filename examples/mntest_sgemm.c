7e0a4ca578f4e67e45c3d810c4556368d298cea2
Ferreira <jojo.ferreira@live.fr>
2017-05-03 23:04:53 +0200

    fonction et test pour gemm

---
+++ examples/mntest_sgemm.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <cblas.h>
+
+#include "mnblas.h"
+
+/*
+  Mesure des cycles
+*/
+#include <x86intrin.h>
+
+#define NBEXPERIMENTS    102
+static long long unsigned int experiments [NBEXPERIMENTS] ;
+
+// #define VECSIZE    32
+#define VECSIZE    1048576
+
+typedef float vfloat  [VECSIZE] __attribute__ ((aligned (16))) ;
+typedef float vdouble [VECSIZE] __attribute__ ((aligned (16))) ;
+
+vfloat vec1, vec2 ;
+
+long long unsigned int average (long long unsigned int *exps)
+{
+  unsigned int i ;
+  long long unsigned int s = 0 ;
+
+  for (i = 2; i < (NBEXPERIMENTS-2); i++)
+    {
+      s = s + exps [i] ;
+    }
+
+  return s / (NBEXPERIMENTS-2) ;
+}
+
+
+void vector_init (vfloat V, float x)
+{
+  register unsigned int i ;
+
+  for (i = 0; i < VECSIZE; i++)
+    V [i] = x ;
+
+  return ;
+}
+
+void vector_print (vfloat V)
+{
+  register unsigned int i ;
+
+  for (i = 0; i < VECSIZE; i++)
+    printf ("%f ", V[i]) ;
+  printf ("\n") ;
+
+  return ;
+}
+
+int main (int argc, char **argv)
+{
+  unsigned long long int start, end ;
+  unsigned long long int residu ;
+  unsigned long long int av ;
+  int exp ;
+  printf("Comparaison pour GEMM entre CBLAS, notre fonction non parallélisée et notre fonction parallelisée\n");
+ /* Calcul du residu de la mesure */
+  start = _rdtsc () ;
+  end = _rdtsc () ;
+  residu = end - start ;
+
+  for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
+    {
+      vector_init (vec1, 1.0) ;
+
+      start = _rdtsc () ;
+
+         cblas_sgemm (VECSIZE, 3.0, vec1, 1, vec2, 1) ;
+
+      end = _rdtsc () ;
+
+      experiments [exp] = end - start ;
+    }
+
+  av = average (experiments) ;
+
+  printf ("cblas_sgemm : nombre de cycles: \t %Ld ;\t GFLOP/s :\t %3.3f\n ", av-residu,(((double) 8 * (double) VECSIZE) / ((double) (av - residu) * (double) 0.38)));
+
+
+  for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
+    {
+      vector_init (vec1, 1.0) ;
+
+      start = _rdtsc () ;
+
+         mncblas_sgemm_noomp (VECSIZE, 3.0, vec1, 1, vec2, 1) ;
+
+      end = _rdtsc () ;
+
+      experiments [exp] = end - start ;
+    }
+
+  av = average (experiments) ;
+
+  printf ("mncblas_sgemm_noomp : nombre de cycles: \t %Ld ;\t GFLOP/s :\t %3.3f\n ", av-residu,(((double) 8 * (double) VECSIZE) / ((double) (av - residu) * (double) 0.38)));
+
+
+  for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
+    {
+      vector_init (vec1, 1.0) ;
+      vector_init (vec2, 2.0) ;
+
+      start = _rdtsc () ;
+
+          mncblas_sgemm_omp (VECSIZE, 3.0, vec1, 1, vec2, 1) ;
+
+      end = _rdtsc () ;
+
+      experiments [exp] = end - start ;
+    }
+
+  av = average (experiments) ;
+
+  // vector_print (vec2) ;
+  printf ("mncblas_sgemm_omp : nombre de cycles: \t %Ld ;\t GFLOP/s :\t %3.3f\n ", av-residu,(((double) 8 * (double) VECSIZE) / ((double) (av - residu) * (double) 0.38)));
+
+
+
+}
--- src/gemm.c
+++ src/gemm.c
@@ -161,7 +161,7 @@ void mncblas_sgemm (
 			#pragma omp parallel for schedule(static)
 			for (k = 0; k < M; k=k+4)
 			{
-				r = r + A [((i + 1) * M) + k    ] * B [(k * M)     + j] ;
+		 		r = r + A [((i + 1) * M) + k    ] * B [(k * M)     + j] ;
 				r = r + A [((i + 1) * M) + k + 1] * B [(k + 1) * M + j] ;
 				r = r + A [((i + 1) * M) + k + 2] * B [(k + 2) * M + j] ;
 				r = r + A [((i + 1) * M) + k + 3] * B [(k + 3) * M + j] ;
@@ -205,6 +205,110 @@ void mncblas_sgemm (
 	return ;
 }
 
+void mncblas_dgemm_1 (
+	MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+	MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+	const int K, const double alpha, const double *A,
+	const int lda, const double *B, const int ldb,
+	const double beta, double *C, const int ldc
+)
+{
+	/*
+	vectorized implementation
+	*/
+
+	register unsigned int i ;
+	register unsigned int j ;
+	register unsigned int k ;
+	register unsigned int l ;
+
+	register unsigned int indice_ligne ;
+
+	register double r ;
+	double *Bcol ;
+	double *R4 ;
+	int      err ;
+
+	__m128d av4 ;
+	__m128d bv4 ;
+	__m128d dot ;
+
+	/*
+	Bcol = (floatM) malloc (M * sizeof (double)) ;
+	err = posix_memalign ((void **) &Bcol, 16, M*sizeof(double)) ;
+	*/
+
+	if (TransB == MNCblasNoTrans)
+	{
+		Bcol = aligned_alloc (16, M * sizeof (double)) ;
+
+		for (i = 0 ; i < M; i = i + 1)
+		{
+
+			for (j = 0 ; j < M; j ++)
+			{
+
+
+				/*
+				load a B column (j)
+				*/
+
+				for (l = 0 ; l < M ; l = l + 2)
+				{
+					Bcol [l]     = B [l        * M + j ] ;
+					Bcol [l + 1] = B [(l + 1)  * M + j ] ;
+				}
+
+				r = 0 ;
+				indice_ligne = i * M ;
+
+				for (k = 0; k < M; k = k + 2)
+				{
+
+					av4 = _mm_load_pd (A+indice_ligne + k);
+					bv4 = _mm_load_pd (Bcol+ k) ;
+
+					dot = _mm_dp_pd (av4, bv4, 0xFF) ;
+
+					_mm_store_pd (R4, dot) ;
+
+					r = r + R4 [0] ;
+				}
+
+				C [indice_ligne + j] = (alpha * r) + (beta * C [indice_ligne + j]) ;
+			}
+		}
+	}
+	else
+	{
+		for (i = 0 ; i < M; i = i + 1)
+		{
+
+			for (j = 0 ; j < M; j ++)
+			{
+
+				r = 0.0 ;
+				indice_ligne = i * M ;
+
+				for (k = 0; k < M; k = k + 2)
+				{
+
+					av4 = _mm_load_pd (A + indice_ligne + k);
+					bv4 = _mm_load_pd (B + indice_ligne + k) ;
+
+					dot = _mm_dp_pd (av4, bv4, 0xFF) ;
+
+					_mm_store_pd (R4, dot) ;
+
+					r = r + R4 [0] ;
+				}
+
+				C [indice_ligne + j] = (alpha * r) + (beta * C [indice_ligne + j]) ;
+			}
+		}
+	}
+	return ;
+}
 
 void mncblas_dgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
 	MNCBLAS_TRANSPOSE TransB, const int M, const int N,
@@ -212,9 +316,84 @@ void mncblas_dgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
 	const int lda, const double *B, const int ldb,
 	const double beta, double *C, const int ldc)
 	{
+	/*
+	scalar implementation
+	*/
 
-		return ;
+	register unsigned int i ;
+	register unsigned int j ;
+	register unsigned int k ;
+	register double r ;
+	#pragma omp parallel for schedule(static)
+	for (i = 0 ; i < M; i = i + 4)
+	{
+		/* i */
+		#pragma omp parallel for schedule(static)
+		for (j = 0 ; j < M; j ++)
+		{
+			r = 0.0 ;
+			#pragma omp parallel for schedule(static)
+			for (k = 0; k < M; k=k+4)
+			{
+				r = r + A [(i * M) + k    ] * B [(k * M)       + j] ;
+				r = r + A [(i * M) + k + 1] * B [(k + 1) * M   + j] ;
+				r = r + A [(i * M) + k + 2] * B [(k + 2) * M   + j] ;
+				r = r + A [(i * M) + k + 3] * B [(k + 3) * M   + j] ;
+			}
+			C [(i*M) + j] = (alpha * r) + (beta * C [(i*M) + j]) ;
+		}
+
+		/* i + 1 */
+		#pragma omp parallel for schedule(static)
+		for (j = 0 ; j < M; j ++)
+		{
+			r = 0.0 ;
+			#pragma omp parallel for schedule(static)
+			for (k = 0; k < M; k=k+4)
+			{
+		 		r = r + A [((i + 1) * M) + k    ] * B [(k * M)     + j] ;
+				r = r + A [((i + 1) * M) + k + 1] * B [(k + 1) * M + j] ;
+				r = r + A [((i + 1) * M) + k + 2] * B [(k + 2) * M + j] ;
+				r = r + A [((i + 1) * M) + k + 3] * B [(k + 3) * M + j] ;
+			}
+			C [((i + 1) * M) + j] = (alpha * r) + (beta * C [((i + 1) * M) + j]) ;
+		}
+
+		/* i + 2 */
+		#pragma omp parallel for schedule(static)
+		for (j = 0 ; j < M; j ++)
+		{
+			r = 0.0 ;
+			#pragma omp parallel for schedule(static)
+			for (k = 0; k < M; k = k + 4)
+			{
+				r = r + A [((i + 2) * M) + k    ] * B [(k * M)     + j] ;
+				r = r + A [((i + 2) * M) + k + 1] * B [(k + 1) * M + j] ;
+				r = r + A [((i + 2) * M) + k + 2] * B [(k + 2) * M + j] ;
+				r = r + A [((i + 2) * M) + k + 3] * B [(k + 3) * M + j] ;
+			}
+			C [((i + 2) * M) + j] = (alpha * r) + (beta * C [((i + 2) * M) + j]) ;
+		}
+
+		/* i + 3 */
+		#pragma omp parallel for schedule(static)
+		for (j = 0 ; j < M; j ++)
+		{
+			r = 0.0 ;
+			#pragma omp parallel for schedule(static)
+			for (k = 0; k < M; k = k + 4)
+			{
+				r = r + A [((i + 3) * M) + k    ] * B [(k * M)     + j] ;
+				r = r + A [((i + 3) * M) + k + 1] * B [(k + 1) * M + j] ;
+				r = r + A [((i + 3) * M) + k + 2] * B [(k + 2) * M + j] ;
+				r = r + A [((i + 3) * M) + k + 3] * B [(k + 3) * M + j] ;
+			}
+			C [((i + 3) * M) + j] = (alpha * r) + (beta * C [((i + 3) * M) + j]) ;
+		}
+
 	}
+	return ;
+}
 
 
 	void mncblas_cgemm (
